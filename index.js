0bf3029dfc1426bee2cab6591f4cc3e1cbd9022a
Катерина Осадчая <monzolevskaya@gmail.com>
2021-07-21 16:50:51 +0300

    to string

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // import './js/variables.js';
-import './js/primitives.js';
+// import './js/primitives.js';
+import './js/toString.js';
 // import './js/module-1-1.js';
---
+++ js/toString.js
@@ -0,0 +1,118 @@
+// ====== TO STRING (приводим к строке)======
+
+
+// ЧИСЛО К СТРОКЕ
+let value = 0;
+
+//1й способ
+let toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+value = 1;
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+//---------------------------------------------------------------------------------------
+
+// ЗНАЧЕНИЕ БЕСКОНЕЧНОСТИ К СТРОКЕ
+
+value = Infinity;
+
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+//--------------------------------------------------------------------------------------
+
+// НЕ ЧИСЛО К СТРОКЕ
+
+value = NaN;
+
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+//--------------------------------------------------------------------------------------
+
+//БУЛЕВОЕ (логическое) true к строке
+value = true;
+
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+
+
+//БУЛЕВОЕ (логическое) false к строке
+value = false;
+
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+//---------------------------------------------------------------------------------------
+
+//UNDEFINED к строке
+value = undefined;
+
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
+
+//---------------------------------------------------------------------------------------
+
+
+//NULL к строке
+value = null;
+
+//1й способ
+toString = String(value);
+console.log(`${value}, через конструктор String(): `, toString);
+console.log(`тип данных ${value}, через конструктор String(): `, typeof toString);
+
+//2й способ (конкатенация)
+toString = value + "";
+console.log(`${value}, через конкатенацию: `, toString);
+console.log(`тип данных ${value}, через конкатенацию: `, typeof toString);
