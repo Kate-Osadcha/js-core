d599d8146ce537a70023015986b85fcbdf670740
Катерина Осадчая <monzolevskaya@gmail.com>
2021-07-21 21:27:49 +0300

    module-1-2

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 // import './js/toString.js';
 // import './js/toNumber.js';
 // import './js/toBoolean.js';
-import './js/false-values.js';
+// import './js/false-values.js';
 
 
 // import './js/module-1-1.js';
+import './js/module-1-2.js';
